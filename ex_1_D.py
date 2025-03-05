1463d1e842feecfadb3b8c62bae34b1e3c6f0972
Gerunia <braineaterofficial7200@gmail.com>
2025-03-05 23:58:43 +0300

    snova kopi na chvoste

--- ex_1_D.py
+++ ex_1_D.py
@@ -1,30 +1,33 @@
-def eratosthenes(n: int) -> list[int]:
-    numbers = list(range(2, n + 1))
-    for number in numbers:
-        if number != 0:
-            for candidate in range(2 * number, n + 1, number):
-                numbers[candidate - 2] = 0
+def update_prime_factors(a, ma):
+    while a % 2 == 0:
+        ma[2] = ma.get(2, 0) + 1
+        a = a // 2
 
-    return list(filter(lambda x: x != 0, numbers))
+    i = 3
+    while i * i <= a:
+        while a % i == 0:
+            ma[i] = ma.get(i, 0) + 1
+            a = a // i
+        i += 2
+
+    if a > 1:
+        ma[a] = ma.get(a, 0) + 1
+
 
 def solve_array() -> str:
-    len_arr = int(input().strip())
-    arr = [int(x) for x in input().strip().split()]
-    primes = eratosthenes(10 ** 6)
-    primes_dict = {key: 0 for key in primes}
+    n = int(input())
+    mp = {}
 
-    for x in arr:
-        num = x
-        while num > 1:
-            for p in primes:
-                if num % p == 0:
-                    primes_dict[p] += 1
-                    num //= p
-                if p > x // 2:
-                    break
+    for x in list(map(int, input().split())):
+        update_prime_factors(x, mp)
 
-    return "YES" if all(value == 0 or value % len_arr == 0 for value in primes_dict.values()) else "NO"
+    ok = True
+    for count in mp.values():
+        if count % n != 0:
+            ok = False
+            break
 
+    return "YES" if ok else "NO"
 
 def main():
     test_count = int(input().strip())
